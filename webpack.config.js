e50df25883c815d98ef4c964ce0aa98b106795c7
Marco Ribeiro <mribeirodev@gmail.com>
2017-02-18 16:04:35 -0200

    Created initial development environment

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const inputDirName = 'app'
+const outputDirName = 'dist'
+const entryFilename = 'index.js'
+
+module.exports = {
+    entry: path.resolve(__dirname, inputDirName, entryFilename),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, outputDirName)
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+}
